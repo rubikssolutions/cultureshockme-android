a1247402d26f86e1556f4714936a582237b7805c
Rauno <rauno@rubikssolutions.com>
2013-07-30 18:04:52 +0300

    Added the pictures from the stories. Changed the UI.

--- src/com/rubikssolutions/cultureshockme/MainActivity.java
+++ src/com/rubikssolutions/cultureshockme/MainActivity.java
@@ -1,7 +1,6 @@
 package com.rubikssolutions.cultureshockme;
 
 import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
@@ -10,16 +9,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import android.R.integer;
 import android.app.Activity;
-import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -28,65 +26,162 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	private static final String API_URL = "http://culture-shock.me/ajax/?act=get_stories_more";
 	private static final String TAG = "MainActivity";
+	private static final Float FLAG_SCALE = 1.5f;
 
-	TextView[] textViewArray;
-	TextView[] authorTextViewsArray;
-	ImageView[] imageViewsArray;
+	TextView[] textViews;
+	TextView[] authorTextViews;
+	ImageView[] flagImageViews;
+	ImageView[] backgroundImageViews;
 
 	int counter = 0;
 
-	int amountOfText = 12; // Should be calculated by pulling data from the
-							// server TODO
+	/*
+	 * pulls the first x stories and displays them.
+	 * Higher values throw an OutOfMemoryError on my crappy phone,
+	 * but work fine on the Genymotion emulator.
+	 * 
+	 * Potential max is 12 for now I think.
+	 */
+	int amountToLoad = 5; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		textViewArray = new TextView[amountOfText];
-		authorTextViewsArray = new TextView[amountOfText];
-		imageViewsArray = new ImageView[amountOfText];
-
-		View myLayout = findViewById(R.id.scrollingLinearView);
-		for (int i = 0; i < amountOfText; i++) {
-			TextView text = new TextView(this);
-			textViewArray[i] = text;
-			TextView author = new TextView(this);
-			authorTextViewsArray[i] = author;
-			ImageView flag = new ImageView(this);
-			imageViewsArray[i] = flag;
-
-			text.setLayoutParams(new LinearLayout.LayoutParams(
-					LinearLayout.LayoutParams.WRAP_CONTENT,
-					LinearLayout.LayoutParams.WRAP_CONTENT));
-			author.setLayoutParams(new LinearLayout.LayoutParams(
-					LinearLayout.LayoutParams.WRAP_CONTENT,
-					LinearLayout.LayoutParams.WRAP_CONTENT));
-			flag.setLayoutParams(new LinearLayout.LayoutParams(
-					LinearLayout.LayoutParams.WRAP_CONTENT,
-					LinearLayout.LayoutParams.WRAP_CONTENT));
-
-			((LinearLayout) myLayout).addView(author);
-			((LinearLayout) myLayout).addView(flag);
-			((LinearLayout) myLayout).addView(text);
-		}
+		
+		addViews();
+		
 		new StoryLoader().execute();
 		new AuthorLoader().execute();
 		new FlagLoader().execute();
+		
+		// Note - takes a while and is probably horribly inefficient. 
+		new BackgroundLoader().execute();
+	}
+	
+	private void addViews () {
+		View myLayout = findViewById(R.id.scrollingLinearView);
+		
+		// Create the arrays
+		textViews = new TextView[amountToLoad];
+		authorTextViews = new TextView[amountToLoad];
+		flagImageViews = new ImageView[amountToLoad];
+		backgroundImageViews = new ImageView[amountToLoad];
+
+		// Set up the LayoutParams
+		LinearLayout.LayoutParams backgroundParams = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.MATCH_PARENT);
+		LinearLayout.LayoutParams layoutParamsBottomPadding = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		layoutParamsBottomPadding.setMargins(0, 0, 0, 15);
+		LinearLayout.LayoutParams layoutParamsNoPadding = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		
+		// Add all the views
+		for (int i = 0; i < amountToLoad; i++) {
+			// Create the new viws
+			TextView text = new TextView(this);
+			TextView author = new TextView(this);
+			ImageView flag = new ImageView(this);
+			ImageView background = new ImageView(this);
+			
+			// Add them to the arrays
+			textViews[i] = text;
+			authorTextViews[i] = author;
+			flagImageViews[i] = flag;
+			backgroundImageViews[i] = background; 
+			
+			// Set the loading text
+			text.setText("Loading stories...");
+			text.setTextSize(20f);
+			author.setText("Loading authors...");
+			
+			// Set the layout parameters
+			author.setLayoutParams(layoutParamsNoPadding);
+			flag.setLayoutParams(layoutParamsNoPadding);
+			flag.setPadding(0, 0, 0, 5);
+			background.setLayoutParams(backgroundParams);
+			background.setAdjustViewBounds(true);			
+			text.setLayoutParams(layoutParamsBottomPadding);
+
+			// Set the background color
+			author.setBackgroundColor(Color.WHITE);
+			background.setBackgroundColor(Color.WHITE);
+			text.setBackgroundColor(Color.WHITE);
+
+			// The order in which to display the items
+			((LinearLayout) myLayout).addView(flag);
+			((LinearLayout) myLayout).addView(author);
+			((LinearLayout) myLayout).addView(background);
+			((LinearLayout) myLayout).addView(text);
+		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	class BackgroundLoader extends AsyncTask<String, Void, Bitmap[]> {
+		@Override
+		protected Bitmap[] doInBackground(String... params) {	
+			Bitmap[] backgroundArray = new Bitmap[amountToLoad];
+			String[] backgroundURLArray = new String[amountToLoad];
+			
+			try {
+				Document doc = Jsoup.connect(API_URL).get();
+				int backgroundCounter = 0;
+				for (int i = 0; i < (amountToLoad * 2); i++) {
+					try {
+						String uRLString = doc.select("[style^=background-image:url(']").get(i) + " a";
+						uRLString = uRLString.substring(73);
+						if (uRLString.startsWith("http")) {
+							uRLString = uRLString.substring(0, uRLString.length() - 12);
+							backgroundURLArray[backgroundCounter] = uRLString;
+							backgroundCounter++;
+						} else if (uRLString.startsWith("'")) {
+							backgroundCounter++;
+						}
+					} catch (Exception e) {
+						Log.e(TAG, "error fetching BACKGROUND from server", e);
+					}
+				}
+			} catch (Exception e) {
+				Log.e(TAG, "error connecting to server", e);
+			}
+			
+			for (int i = 0; i < backgroundArray.length; i++) {
+				try {
+					Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(backgroundURLArray[i]).getContent());
+					backgroundArray[i] = bitmap; 
+				} catch (Exception e) {
+					Log.e(TAG, "error fetching BACKGROUND from URL", e);
+				}
+			}
+			return backgroundArray;
+		}
+		
+		@Override
+		protected void onPostExecute(Bitmap[] backgroundArray) {
+			for (int i = 0; i < backgroundImageViews.length; i++) {
+				try {
+					backgroundArray[i] = scaleBitmapToDevice(backgroundArray[i]);
+					backgroundImageViews[i].setImageBitmap(backgroundArray[i]);
+				} catch (Exception e) {
+					Log.e(TAG, "Can't scale Bitmap, probable cause: picture missing from the story!", e);
+				}
+			}
+		}
 	}
 
 	class StoryLoader extends AsyncTask<String, Void, String[]> {
+		protected void onPreExecute() {
+			textViews[0].setTextSize(25f);
+			textViews[0].setText("Loading stories...");
+		}
+		
 		protected String[] doInBackground(String... urls) {
 			try {
 				Document doc = Jsoup.connect(API_URL).get();
-				String[] textArray = new String[amountOfText];
+				String[] textArray = new String[amountToLoad];
 
 				for (int i = 0; i < textArray.length; i++) {
 					Element textElement = doc.select("H3").get(i);
@@ -101,9 +196,11 @@ public class MainActivity extends Activity {
 		}
 
 		protected void onPostExecute(String[] result) {
-			for (int i = 0; i < textViewArray.length; i++) {
-				textViewArray[i].setText(result[i]);
-				textViewArray[i].setTextSize(17f);
+			if (result != null) {
+				for (int i = 0; i < textViews.length; i++) {
+					textViews[i].setText(result[i]);
+					textViews[i].setTextSize(17f);
+				}
 			}
 		}
 	}
@@ -112,13 +209,13 @@ public class MainActivity extends Activity {
 		protected String[] doInBackground(String... urls) {
 			try {
 				Document doc = Jsoup.connect(API_URL).get();
-				String[] authorArray = new String[amountOfText];
+				String[] authorArray = new String[amountToLoad];
 
-				for (int i = 0; i < authorArray.length; i++) {
+				for (int i = 0; i < authorArray.length; i++) {							
 					Element authorElement = doc.select("[class=user_link]").get(i);
-					authorArray[i] = authorElement.text();
+					Element countryElement = doc.select("[class=browse_story_location with_countryflag_icon]").get(i);
+					authorArray[i] = authorElement.text() + " - " +  countryElement.text();
 				}
-
 				return authorArray;
 			} catch (Exception e) {
 				Log.e(TAG, "error fetching AUTHOR from server", e);
@@ -127,20 +224,21 @@ public class MainActivity extends Activity {
 		}
 
 		protected void onPostExecute(String[] result) {
-			for (int i = 0; i < authorTextViewsArray.length; i++) {
-				authorTextViewsArray[i].setText(result[i]);
-				authorTextViewsArray[i].setTextColor(Color.RED);
-				authorTextViewsArray[i].setTextSize(15f);
+			if (result != null) {
+				for (int i = 0; i < authorTextViews.length; i++) {
+					authorTextViews[i].setText(result[i]);
+					authorTextViews[i].setTypeface(null, Typeface.ITALIC);
+					authorTextViews[i].setTextSize(15f);
+				}
 			}
 		}
 	}
 
 	class FlagLoader extends AsyncTask<String, Integer, Bitmap[]> {
-
 		protected Bitmap[] doInBackground(String... params) {
 			try {
 				Document doc = Jsoup.connect(API_URL).get();
-				Bitmap[] flagArray = new Bitmap[amountOfText];
+				Bitmap[] flagArray = new Bitmap[amountToLoad];
 
 				for (int i = 0; i < flagArray.length; i++) {
 					Element imageElement = doc.select("[style*=flags/mini]").get(i);
@@ -164,9 +262,25 @@ public class MainActivity extends Activity {
 		}
 
 		protected void onPostExecute(Bitmap[] result) {
-			for (int i = 0; i < imageViewsArray.length; i++) {
-				imageViewsArray[i].setImageBitmap(result[i]);
+			if (result != null) {
+				for (int i = 0; i < flagImageViews.length; i++) {
+					flagImageViews[i].setImageBitmap(result[i]);
+					flagImageViews[i].setScaleX(FLAG_SCALE);
+					flagImageViews[i].setScaleY(FLAG_SCALE);
+				}
 			}
 		}
+	}
+
+	private Bitmap scaleBitmapToDevice(Bitmap inputBitmap) { 
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int deviceWidth = dm.widthPixels;
+		
+		float width = inputBitmap.getWidth();
+		float height = inputBitmap.getHeight();
+		float ratio = (width / height);
+		
+		return Bitmap.createScaledBitmap(inputBitmap, deviceWidth, (int)(deviceWidth / ratio), false);
 	}
 }
